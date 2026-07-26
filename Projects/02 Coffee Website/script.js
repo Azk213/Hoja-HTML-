445ff0a9ec86d21252de0081390aa90921d0aab7
Azk213 <2013.azimahmed@gmail.com>
2026-07-26 14:55:20 +0530

    Update

---
+++ Projects/02 Coffee Website/script.js
@@ -0,0 +1,5 @@
+const menuOpenButton = document.querySelector("#menu-open-button ");
+
+menu.OpenButton.addEventListener("click", ()  => {
+    document.body.classList.toggle("show-mobile-menu")
+})
