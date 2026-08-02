ad9ecc60e9b67fb0984bef5fc1c10d150e0ef333
Azk213 <2013.azimahmed@gmail.com>
2026-08-02 13:18:19 +0530

    Update

--- Projects/02 Coffee Website/script.js
+++ Projects/02 Coffee Website/script.js
@@ -1,8 +1,15 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 
 menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+menuCloseButton.addEventListener("click", () => {
+    menuOpenButton.click();
+});
+
+navLinks.forEach(link =>{
+    link.addEventListener("click" , () => menuOpenButton.click());
+});
