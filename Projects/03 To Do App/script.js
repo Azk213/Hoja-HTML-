001917c9369ee1e46cab26bbb15ec34b760a7e24
Azk213 <2013.azimahmed@gmail.com>
2026-08-14 19:57:56 +0530

    Update

---
+++ Projects/03 To Do App/script.js
@@ -0,0 +1,79 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const taskCount = document.getElementById("taskCount");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+renderTasks();
+
+function addTask() {
+    const text = taskInput.value.trim();
+
+    if (!text) return;
+
+    tasks.push({ text, completed: false });
+    taskInput.value = "";
+
+    saveTask();
+}
+
+taskInput.addEventListener("keypress", e => {
+    if (e.key === "Enter") addTask();
+});
+
+function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+}
+
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+
+        if (task.completed) {
+            li.classList.add("completed");
+        }
+
+        li.innerHTML = `
+        <span onclick="toggleTask(${index})">${task.text}</span>
+
+        <div class="task-buttons">
+            <button onclick="editTask(${index})">✒️</button>
+            <button onclick="deleteTask(${index})">🗑️</button>
+        </div>`;
+
+        taskList.appendChild(li);
+    });
+
+    taskCount.textContent = `${tasks.length} tasks`;
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    saveTask();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    saveTask();
+}
+
+function editTask(index) {
+    const newText = prompt("Edit Task: ", tasks[index].text);
+
+    if (newText !== null) {
+        tasks[index].text = newText.trim();
+        saveTask();
+    }
+}
+
+function clearCompleted() {
+    tasks = tasks.filter(task => !task.completed);
+    saveTask();
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
+}
